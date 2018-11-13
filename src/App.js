d3d9563c57079c97e75eaeba0c10f573df6954bb
YashBhatia97 <iit2015105@iiita.ac.in>
2018-11-13 11:40:14 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+
+const API_KEY = 'AIzaSyCx_f1nFUCtC0Rf8tqOKKiXvyfKXcag-0c';
+
+class App extends Component {
+
+  constructor (props){
+    super (props);
+
+    this.state = {
+      videos: []
+    };
+
+    this.videoSearch('React Tutorials');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (data) => {
+      //console.log(data);
+      this.setState({videos : data})
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+export default App;
